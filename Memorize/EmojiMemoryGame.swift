95c6be09eece0b9fedb5dcbf5fc4439355eb721e
Rishi Jansari <jansaririshi@gmail.com>
2024-06-30 19:40:55 +0100

    Lecture 3-5, removed hw task and reverted to before - **Added logic of game using models** Partial completion, logic of game works using models Added animation of shuffling and the shuffle button Added viewModel to game Renamed content view to emojimemorygameview

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Rishi Jansari on 30/06/2024.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["🍏", "🍐", "🍊", "🍋", "🍓", "🍇", "🍉", "🍌", "🫐", "🍍", "🥥", "🍒", "🥭", "🥝", "🍑"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 15) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "!?"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
